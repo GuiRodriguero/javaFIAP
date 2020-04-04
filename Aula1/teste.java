d1dba6366c449ce72deeadeb22c10fb77951091e
GuiRodriguero <50684900+GuiRodriguero@users.noreply.github.com>
2020-04-03 23:56:07 -0300

    Add files via upload

---
+++ Aula1/teste.java
@@ -0,0 +1,9 @@
+package Aula1;
+//show array without index
+public class teste {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        System.out.print(arr);
+    }
+
+}
